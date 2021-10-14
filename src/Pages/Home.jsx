d39e8faa4c35b25bb0eb4ccecc801b295b19f4b0
dheerajjha97 <jhadheeraj97@gmail.com>
2021-10-14 18:11:27 +0530

    2 files modified

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home=()=>{
 return(
-  <div className="flex mx-auto h-96 bg-red-600">
+  <div className="flex mx-auto h-96 bg-red-600 text-cente">
     hell
     </div>
 )
